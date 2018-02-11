9a50ae2a58a3ae169f3c057cf0beea845791adf9
Andres <andresguisado1@gmail.com>
2018-02-11 22:29:09 +0000

    Modifying docker/aks files

--- docker/aks/create_cluster.sh
+++ docker/aks/create_cluster.sh
@@ -1,4 +1,4 @@
-docker run --name azure-k8s-cli-docker -v $REPO_PATH:/home --env-file $ENV_VAR_PATH --rm -it \
+docker run --name azure-k8s-cli-docker -v $REPO_PATH:/opt --env-file $ENV_VAR_PATH --rm -it \
     microsoft/azure-cli \
     sh -c "az login -u \${CLIENT_ID} -p \${CLIENT_SECRET} > /dev/null; \
            cd /opt/azure/cli/aks && sh create_cluster.sh;"
